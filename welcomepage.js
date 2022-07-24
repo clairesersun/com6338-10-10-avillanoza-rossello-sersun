c22d0ced10981b6d7d984da6f5979ffaf7c7618c
Claire Sersun <claire.sersun@gmail.com>
2022-07-23 21:25:11 -0400

    loader page css

--- welcomepage.js
+++ welcomepage.js
@@ -3,4 +3,3 @@ document.getElementById("inputName").innerHTML = localStorage.getItem('nameInput
 setTimeout(function(){
     window.location.replace("Workouts Page.html");            
 }, 3000);
-   
